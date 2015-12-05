0b2ec2f7d5a9370738f06f0adf6ea0ff71d932e2
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-12-04 18:54:25 -0600

    Working solution for 'total ribbon required' for part 2

--- Day2/day2.rb
+++ Day2/day2.rb
@@ -5,6 +5,7 @@ class PresentList
 
     create_presents
     puts total_paper_required
+    puts total_ribbon_required
   end
 
   def create_presents
@@ -23,39 +24,78 @@ class PresentList
 
     paper_total
   end
+
+  def total_ribbon_required
+    ribbon_total = 0
+
+    @presents.each do |present|
+      ribbon_total += present.required_ribbon
+    end
+
+    ribbon_total
+  end
 end
 
 class Present
-  attr_accessor :required_paper
+  attr_accessor :required_paper, :required_ribbon
 
   def initialize(present_dimensions)
     @length = present_dimensions[0]
     @width = present_dimensions[1]
     @height = present_dimensions[2]
-    @sides = determine_sides
+    @areas_of_sides = determine_areas_of_sides
+    @perimeters_of_sides = determine_perimeters_of_sides
     @required_paper = calculate_required_paper
+    @required_ribbon = calculate_required_ribbon
   end
 
-  def determine_sides
-    sides = []
-    sides << area_of_side(@length, @width)
-    sides << area_of_side(@width, @height)
-    sides << area_of_side(@height, @length)
-    sides
+  def determine_areas_of_sides
+    areas = []
+    areas << area_of_side(@length, @width)
+    areas << area_of_side(@width, @height)
+    areas << area_of_side(@height, @length)
+    areas
+  end
+
+  def determine_perimeters_of_sides
+    perimeters = []
+    perimeters << perimeter_of_side(@length, @width)
+    perimeters << perimeter_of_side(@width, @height)
+    perimeters << perimeter_of_side(@height, @length)
+    perimeters
   end
 
   def calculate_required_paper
-    2*(@sides.inject(:+)) + area_of_smallest_side
+    2*(@areas_of_sides.inject(:+)) + area_of_smallest_side
   end
 
   def area_of_side(a, b)
     a * b
   end
 
+  def perimeter_of_side(a, b)
+    2 * a + 2 * b
+  end
+
   def area_of_smallest_side
-    @sides.min
+    @areas_of_sides.min
+  end
+
+  def smallest_perimeter
+    @perimeters_of_sides.min
+  end
+
+  def calculate_required_ribbon
+    smallest_perimeter + volume
+  end
+
+  def volume
+    @length * @width * @height
   end
 
 end
 
 list = PresentList.new('present_list.txt')
+
+# ham = Present.new([1, 1, 10])
+# puts ham.required_ribbon
