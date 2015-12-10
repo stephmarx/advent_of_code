dba533d885f18460288d787f74fe63d2ab68316a
stephmarx <stephanie.n.marx@gmail.com>
2015-12-10 14:26:10 -0600

    Add solution for part 2 with robo-santa

--- Day3/day3.rb
+++ Day3/day3.rb
@@ -6,16 +6,55 @@ class Trip
     @houses = []
     @position = [0, 0]
 
+    @santa = Person.new
+    @robo = Person.new
+
     @houses << House.new([0, 0])
     deliver_presents
     print_house_count
   end
 
   def deliver_presents
-    @instructions.split("").each do |char|
-      move_position(char)
-      deliver_present_to_house
+    @instructions.split("").each_with_index do |char, index|
+      if index.even?
+        @santa.move_position(char)
+        deliver_present_to_house(@santa.position)
+      else
+        @robo.move_position(char)
+        deliver_present_to_house(@robo.position)
+      end
     end
+  end
+
+
+  def deliver_present_to_house(position)
+    @houses.each do |house|
+      if house.position == position
+        house.deliver_present
+        return nil
+      end
+    end
+
+    @houses << House.new(position.clone)
+  end
+
+  def print_manifest
+    @houses.each do |house|
+      puts "The house at #{house.position} has #{house.presents} presents."
+    end
+  end
+
+  def print_house_count
+    puts "#{@houses.length} houses received at least one present."
+  end
+
+end
+
+class Person
+  attr_accessor :position
+
+  def initialize
+    @position = [0, 0]
   end
 
   def move_position(char)
@@ -29,27 +68,6 @@ class Trip
     when "v"
       @position[1] -= 1
     end
-  end
-
-  def deliver_present_to_house
-    @houses.each do |house|
-      if house.position == @position
-        house.deliver_present
-        return nil
-      end
-    end
-
-    @houses << House.new(@position.clone)
-  end
-
-  def print_manifest
-    @houses.each do |house|
-      puts "The house at #{house.position} has #{house.presents} presents."
-    end
-  end
-
-  def print_house_count
-    puts "#{@houses.length} houses received at least one present."
   end
 
 end
